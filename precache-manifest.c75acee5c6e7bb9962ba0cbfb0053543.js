46f0eabfb4a9f6b2559e21761b530c29776a6fd7
TamaraJammoul <tamara.jamoul@gmail.com>
2020-05-10 00:10:01 +0300

    Updates

---
+++ precache-manifest.c75acee5c6e7bb9962ba0cbfb0053543.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a09887715d93614f61e973639f38d418",
+    "url": "/React-Ecommerce/index.html"
+  },
+  {
+    "revision": "4a6bc814398a94d926d7",
+    "url": "/React-Ecommerce/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "9cc20b8fd715788f52a1",
+    "url": "/React-Ecommerce/static/css/main.15cd4cc8.chunk.css"
+  },
+  {
+    "revision": "4a6bc814398a94d926d7",
+    "url": "/React-Ecommerce/static/js/2.e78d2a2e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/React-Ecommerce/static/js/2.e78d2a2e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9cc20b8fd715788f52a1",
+    "url": "/React-Ecommerce/static/js/main.a771928d.chunk.js"
+  },
+  {
+    "revision": "5b6c2093f15cfd18a3bd",
+    "url": "/React-Ecommerce/static/js/runtime-main.a1960754.js"
+  },
+  {
+    "revision": "9f7e506d7455da27ce24e474c84a8e0a",
+    "url": "/React-Ecommerce/static/media/logo.9f7e506d.svg"
+  }
+]);
